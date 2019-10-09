eed7b4433086b240d1467aa31dfbb9b6de9f2e30
alvaro <amartinezfernandez.61@gmail.com>
2019-10-09 15:47:35 +0200

    block class

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hola mundo v2")
+import PKG from './package';
+import Block from './src/blockchain/block';
+
+const { name, version } = PKG;
+
+console.log(`${name} v${version}`);
+
+const block = new Block(Date.now(), 'previoushHash', 'hash', 'data')
+console.log(block.toString())
